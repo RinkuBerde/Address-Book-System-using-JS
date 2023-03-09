8fea7ae3b7bec6a2a4ec43c1e4880b2d0ded886e
rinkuberde <rinkuberde@gmail.com>
2023-03-09 20:05:05 +0530

    SortByCityStateOrZip

--- AddressBook/Address_Book.js
+++ AddressBook/Address_Book.js
@@ -7,11 +7,9 @@ const zip = RegExp("^[0-9]{3}\\s{0,1}[0-9]{3}$");
 const phoneNumber = RegExp("^[0-9]{2}[-]{1}[0-9]{10}$");
 const email = RegExp("^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$");
 
-// UC-03----->Using array to store the contacts.
-let addressBookArray = new Array();
 class Contact {
     // Constructor
-    // @param  {...any} param - above mentioned fields.   
+    // @param  {...any} param - above mentioned fields.    
     constructor(...params) {
       this.firstName = params[0];
       this.lastName = params[1];
@@ -21,7 +19,6 @@ class Contact {
       this.phoneNumber = params[5];
       this.email = params[6];
     }
-
     // Getters for all fields
     get firstName() {
       return this._firstName;
@@ -50,8 +47,8 @@ class Contact {
     get email() {
       return this._email;
     }
-   
-    // Setters for all fields
+  
+     //Setters for all fields
     set firstName(firstName) {
       if (name.test(firstName)) this._firstName = firstName;
       else throw "**** FIRST NAME is Incorrect ****";
@@ -91,9 +88,8 @@ class Contact {
       if (email.test(email)) this._email = email;
       else throw "**** EMAIL ADDRESS is Incorrect ****";
     }
-
+   
      //To String Method
-     // @returns - Contact object in String form.     
     toString() {
       return (
         "First Name : " +
@@ -114,15 +110,57 @@ class Contact {
     }
   }
   
-//UC-04---->Function to check if the contact exists in array.
-function contactExists(firstName, lastName) {
+ // Using array to store the contacts.
+// We are using the push method to add the contact in the array
+  let addressBookArray = new Array();
+  
+
+   // Function to check if the contact exists in array.
+  function contactExists(firstName, lastName) {
     return addressBookArray.some(
       (contact) => contact.firstName == firstName && contact.lastName == lastName
     );
   }
+  
+  
+   // Function to edit the contact
+  function editContact(firstName, lastName, property, newValue) {
+    if (contactExists(firstName, lastName)) {
+      switch (property) {
+        case "city":
+          addressBookArray.find(
+            (contact) => contact.firstName == firstName
+          ).city = newValue;
+          break;
+        case "state":
+          addressBookArray.find(
+            (contact) => contact.firstName == firstName
+          ).state = newValue;
+          break;
+        case "zip":
+          addressBookArray.find((contact) => contact.firstName == firstName).zip =
+            newValue;
+          break;
+        case "phoneNumber":
+          addressBookArray.find(
+            (contact) => contact.firstName == firstName
+          ).phoneNumber = newValue;
+          break;
+        case "email":
+          addressBookArray.find(
+            (contact) => contact.firstName == firstName
+          ).email = newValue;
+          break;
+        default:
+          console.log("Enter valid property");
+      }
+    } else {
+      console.log("Contact Does Not Exist");
+    }
+  }
 
- //UC-05---->Function to delete a contact
-function deleteContact(firstName, lastName) {
+   // Function to delete a contact
+  function deleteContact(firstName, lastName) {
     if (contactExists(firstName, lastName)) {
       addressBookArray = addressBookArray.filter(
         (contact) =>
@@ -131,10 +169,45 @@ function deleteContact(firstName, lastName) {
     } else {
       console.log("Contact Does Not Exist");
     }
-}
-
-//UC-06----> Function to count the contacts in the array
-function getCountOfContacts(count) {
+  }
+  
+   // Add Cotact to Array
+   
+  try {
+    addressBookArray.push(
+      new Contact(
+        "Rinku",
+        "Berde",
+        "Mumbai",
+        "Maharashtra",
+        "410387",
+        "91-9876543219",
+        "rinku@gmail.com"
+      )
+    );
+  } catch (e) {
+    console.error(e);
+  }
+  try {
+    addressBookArray.push(
+      new Contact(
+        "Omkar",
+        "Mane",
+        "Pune",
+        "Maharashtra",
+        "410387",
+        "91-9876543219",
+        "omkar@gmail.com"
+      )
+    );
+  } catch (e) {
+    console.error(e);
+  }
+  
+   // Function to count the contacts in the array
+   // We have used the reduce method to call the specified function for all the elements in an array.
+   
+  function getCountOfContacts(count) {
     count += 1;
     return count;
   }
@@ -143,6 +216,23 @@ function getCountOfContacts(count) {
     "\nCount of Contacts : " + addressBookArray.reduce(getCountOfContacts, 0)
   );
 
+   // Function to search contact by city  
+  function searchContactByCity(city) {
+    return addressBookArray.filter((contact) => contact.city == city);
+  }
+  
+   //  Function to get count by city
+   // We are using the filter method and length to get the count.
+  function getCountOfContactsByCity(city) {
+    return (
+      "Count of Contacts in " +
+      city +
+      " is: " +
+      addressBookArray.filter((contact) => contact.city == city).length
+    );
+  }
+  
+   //Function to sort the array in asscending order.
   function sortAddressBookByCity() {
     addressBookArray.sort((firstPerson, secondPerson) =>
       firstPerson.city.localeCompare(secondPerson.city)
@@ -156,4 +246,17 @@ function getCountOfContacts(count) {
     );
     console.log(addressBookArray);
 }
- 
+function sortAddressBookByState() {
+    addressBookArray.sort((firstPerson, secondPerson) =>
+      firstPerson.state.localeCompare(secondPerson.state)
+    );
+    console.log(addressBookArray);
+  }
+  
+  function sortAddressBookByZip() {
+    addressBookArray.sort((firstPerson, secondPerson) =>
+      firstPerson.zip.localeCompare(secondPerson.zip)
+    );
+    console.log(addressBookArray);
+  }
+  sortAddressBookByName();
