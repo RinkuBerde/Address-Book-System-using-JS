f47f0bf5f25f80aba78128b9bd56f9484a412d37
rinkuberde <rinkuberde@gmail.com>
2023-03-09 14:42:15 +0530

    Ability to find a person with name delete it from the array

--- AddressBook/Address_Book.js
+++ AddressBook/Address_Book.js
@@ -1,4 +1,5 @@
 console.log("Welcome to Address-Book-System-using-JS")
+//UC-02--->Ability to ensure Valid Contacts are added... 
 const name = RegExp("^[A-Z]{1}[a-z]{2,}$");
 const address = RegExp("^[a-zA-z0-9#,]{4,}$");
 const cityState = RegExp("^[a-zA-z]{4,}$");
@@ -6,12 +7,24 @@ const zip = RegExp("^[0-9]{3}\\s{0,1}[0-9]{3}$");
 const phoneNumber = RegExp("^[0-9]{2}[-]{1}[0-9]{10}$");
 const email = RegExp("^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$");
 
-// Using array to store the contacts.
+// UC-03----->Using array to store the contacts.
 let addressBookArray = new Array();
 
-//Function to check if the contact exists in array.
+//UC-04---->Function to check if the contact exists in array.
 function contactExists(firstName, lastName) {
     return addressBookArray.some(
       (contact) => contact.firstName == firstName && contact.lastName == lastName
     );
+  }
+
+ //UC-05---->Function to delete a contact
+function deleteContact(firstName, lastName) {
+    if (contactExists(firstName, lastName)) {
+      addressBookArray = addressBookArray.filter(
+        (contact) =>
+          contact.firstName != firstName && contact.lastName != lastName
+      );
+    } else {
+      console.log("Contact Does Not Exist");
+    }
   }
