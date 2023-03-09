5e4cad1ace7796f2311e76b79be6e472fbf795a0
rinkuberde <rinkuberde@gmail.com>
2023-03-09 12:03:57 +0530

    create a New Address Book array

--- AddressBook/Address_Book.js
+++ AddressBook/Address_Book.js
@@ -5,3 +5,6 @@ const cityState = RegExp("^[a-zA-z]{4,}$");
 const zip = RegExp("^[0-9]{3}\\s{0,1}[0-9]{3}$");
 const phoneNumber = RegExp("^[0-9]{2}[-]{1}[0-9]{10}$");
 const email = RegExp("^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$");
+
+// Using array to store the contacts.
+let addressBookArray = new Array();
